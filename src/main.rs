c260b29c3d9adb3df93ac074d75da2b5d497b0c2
Advaith <adv@Advaiths-MacBook-Pro.local>
2024-11-25 14:42:59 +0530

    print lines

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::io;
 
 fn main() {
     println!("print your guess!");
+    println!("hello")
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line lol")
-        
+
 }
